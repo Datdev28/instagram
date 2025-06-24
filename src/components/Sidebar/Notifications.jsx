5d930d3bb21a52dc97b9161dc636f5e260c896e7
Datdev28 <nguyenquangdat2842003@gmail.com>
2025-06-24 13:25:38 +0700

    xong UI hiển thị thông báo cho client

--- src/components/Sidebar/Notifications.jsx
+++ src/components/Sidebar/Notifications.jsx
@@ -1,14 +1,18 @@
 import { IoMdHeartEmpty } from "react-icons/io";
 import searchToggleStore from "../../store/searchToggleStore";
-
+import { LuDot } from "react-icons/lu";
+import useNotificationStore from "../../store/useNotificationStore";
 const Notifications = () => {
   const { isOpenToggle, setIsOpenToggle, setMode, mode } = searchToggleStore();
+  const {hasNewNoti, setHasNewNoti} = useNotificationStore()
   const handleClickNotification = () => {
     if (mode === "notifications" && isOpenToggle) {
       setIsOpenToggle(false);
+      setHasNewNoti(false);
     } else {
       setIsOpenToggle(true);
       setMode("notifications");
+      setHasNewNoti(false);
     }
   };
   return (
@@ -16,7 +20,12 @@ const Notifications = () => {
       className="flex items-center max-lg:justify-between gap-5 cursor-pointer hover:bg-color-dash duration-200 rounded-sm py-2 px-2"
       onClick={handleClickNotification}
     >
+      <div className="relative">
       <IoMdHeartEmpty className={`text-3xl  ${mode === 'notifications' && isOpenToggle ? "border border-white rounded-md" : "border-transparent"}`}/>
+       {hasNewNoti && (
+        <LuDot className="absolute top-[-17px] right-[-20px] text-red-600 text-5xl"/>
+       )}
+      </div>
       {!isOpenToggle && <p className={`max-lg:hidden`}>Thông báo</p>}
     </div>
   );
