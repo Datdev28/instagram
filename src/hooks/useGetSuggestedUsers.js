cc5054a3d5261a38f540f8877466452ce213f456
Datdev28 <nguyenquangdat2842003@gmail.com>
2025-06-29 15:45:30 +0700

    xong phần lọc gợi ý user

--- src/hooks/useGetSuggestedUsers.js
+++ src/hooks/useGetSuggestedUsers.js
@@ -4,10 +4,12 @@ import { fireStore } from "../firebase/firebase";
 import useAuthStore from "../store/authStore";
 import { toast } from "react-toastify";
 import useLoadingBarStore from "../store/loadingBarStore";
+import useBlockListStore from "../store/blockListStore";
 
 const useGetSuggestedUsers = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [suggestedUsers, setSuggestedUsers] = useState([]);
+  const { blockerIdList, blockedIdList } = useBlockListStore();
   const { user } = useAuthStore();
   const setProgress = useLoadingBarStore(state => state.setProgress);
   useEffect(() => {
@@ -18,14 +20,14 @@ const useGetSuggestedUsers = () => {
         const snapshot = await getDocs(collection(fireStore, "users"));
         const allUsers = snapshot.docs.map((doc) => doc.data());
 
-        const blockedUserIds = user.blackList;
         const followingIds = user.following;
 
         const filteredUsers = allUsers.filter(
           (u) =>
             u.uid !== user.uid &&
             !followingIds?.includes(u.uid) &&
-            !blockedUserIds?.includes(u.uid)
+            !blockerIdList?.includes(u.uid) &&
+            !blockedIdList?.includes(u.uid)
         );
         setSuggestedUsers(filteredUsers.slice(0, 10));
       } catch (error) {
