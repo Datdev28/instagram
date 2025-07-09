b780c9443291cb3a21526f455bf168e4d71e5088
Datdev28 <nguyenquangdat2842003@gmail.com>
2025-07-09 22:15:57 +0700

    fix xong icon đổi in đậm khi click vào sidebar

--- src/components/Sidebar/Explore.jsx
+++ src/components/Sidebar/Explore.jsx
@@ -1,15 +1,25 @@
-import { MdOutlineExplore } from "react-icons/md";
+import { MdOutlineExplore, MdExplore } from "react-icons/md";
 import searchToggleStore from "../../store/searchToggleStore";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+
 const Explore = () => {
   const { isOpenToggle } = searchToggleStore();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const isActive = location.pathname === "/explore";
+
   return (
-    <div className="flex items-center max-lg:justify-between gap-5 cursor-pointer hover:bg-color-dash duration-200 rounded-sm py-2 px-2"
-     onClick={() => navigate('/explore')}
+    <div
+      className="flex items-center max-lg:justify-between gap-5 cursor-pointer hover:bg-color-dash duration-200 rounded-sm py-2 px-2"
+      onClick={() => navigate("/explore")}
     >
-      <MdOutlineExplore className="text-3xl" />
-      {!isOpenToggle && <p className={`max-lg:hidden`}>Khám phá</p>}
+      {isActive ? (
+        <MdExplore className="text-3xl" />
+      ) : (
+        <MdOutlineExplore className="text-3xl" />
+      )}
+      {!isOpenToggle && <p className="max-lg:hidden">Khám phá</p>}
     </div>
   );
 };
--- src/components/Sidebar/Home.jsx
+++ src/components/Sidebar/Home.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { MdHomeFilled } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { GoHome } from "react-icons/go";
+import { Link, useLocation } from "react-router-dom";
 import searchToggleStore from "../../store/searchToggleStore";
 import userProfileStore from "../../store/userProfileStore";
+
 const Home = () => {
-  const {setUserProfile} = userProfileStore();
+  const { setUserProfile } = userProfileStore();
   const { isOpenToggle, setIsOpenToggle } = searchToggleStore();
+  const location = useLocation();
+
+  const isActive = location.pathname === "/";
+
   const handleClick = () => {
     window.scrollTo({ top: 0 });
     setUserProfile(null);
     setIsOpenToggle(false);
   };
+
   return (
     <Link to="/" onClick={handleClick}>
       <div className="flex items-center max-lg:justify-between rounded-sm gap-5 cursor-pointer hover:bg-color-dash duration-200 py-2 px-2">
-        <MdHomeFilled className="text-3xl" />
+        {isActive ? (
+          <MdHomeFilled className="text-3xl" />
+        ) : (
+          <GoHome className="text-3xl" />
+        )}
         {!isOpenToggle && <p className="max-lg:hidden">Trang chủ</p>}
       </div>
     </Link>
